cec1011335bd06dfa18a688561a7933c9c2a62e9
Furkan Küçük <furkankucuk146@gmail.com>
2023-09-05 23:35:37 +0300

    -

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleUI
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
-
             }
         }
     }
