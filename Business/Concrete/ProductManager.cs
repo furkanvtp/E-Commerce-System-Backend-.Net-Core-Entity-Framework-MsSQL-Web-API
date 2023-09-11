25384f13d199ea381a611d8ce5d63dddd97dc181
Furkan Küçük <furkankucuk146@gmail.com>
2023-09-11 23:52:56 +0300

    Core.Utilties katmanı eklendi. Business katmanında düzenlemeler yapıldı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -20,15 +22,32 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IResult Add(Product product)
         {
-            //İş Kodları
-            return _productDal.GetAll();
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                //magic string:elle girilen standart olmayan mesajlara denir.
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAll()
+        {
+            //İş Kodları
+            return new DataResult(_productDal.GetAll());
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success)   //this demek class ın kendisi yani Result
+        {                                                           //demek. Result ın tek parametreli olan
+            Message = message;                                      //constructorına success i yolla diyorum.
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
