a27f22d46c93c84af1b3ffaa69fcc111943b2c36
Furkan Küçük <furkankucuk146@gmail.com>
2023-09-25 01:07:21 +0300

    Frontend baglantisi düzenlendi.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,7 +45,8 @@ namespace WebAPI
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();//IProductDal çağırdığında
             //                                                   //EfProductDal new'le arka planda
-            
+
+            services.AddCors();
             
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -81,6 +82,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
